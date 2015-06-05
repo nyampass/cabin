a56cb84fab4430fc465c03fa01c42b6d53c936f5
OHTA Shogo <athos0220@gmail.com>
2015-06-05 11:17:59 +0900

    First commit

---
+++ src/cabin/core.clj
@@ -0,0 +1,24 @@
+(ns cabin.core
+  (:require
+    [cabin.handler :refer [app init destroy]]
+    [aleph.http :as http]
+    [ring.middleware.reload :as reload]
+    [environ.core :refer [env]]
+    [taoensso.timbre :as timbre])
+  (:gen-class))
+
+(defn parse-port [[port]]
+  (Integer/parseInt (or port (env :port) "3000")))
+
+(defn -main [& args]
+  "e.g. lein run 3000"
+  (let [port (parse-port args)]
+    (try
+      (init)
+      (.addShutdownHook (Runtime/getRuntime) (Thread. destroy))
+      (http/start-server
+        (if (env :dev) (reload/wrap-reload #'app) app)
+        {:port port})
+      (timbre/info "server started on port:" port)
+      (catch Throwable t
+        (timbre/error (str "server failed to start on port: " port) t)))))
