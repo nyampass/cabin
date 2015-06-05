4fe2249fd4fedd2aca4cf9f70b548402ddb05c43
OHTA Shogo <athos0220@gmail.com>
2015-06-05 16:39:16 +0900

    Tidy up project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,6 @@
 (defproject cabin "0.1.0-SNAPSHOT"
-
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-
+  :url "https://github.com/nyampass/cabin"
   :dependencies [[org.clojure/clojure "1.7.0-RC1"]
                  [selmer "0.8.2"]
                  [com.taoensso/timbre "3.4.0"]
@@ -17,42 +15,25 @@
                  [bouncer "0.3.3"]
                  [prone "0.8.2"]
                  [org.clojure/tools.nrepl "0.2.10"]
-                 
                  [com.novemberain/monger "2.0.1"]
                  [aleph "0.4.0"]]
-
   :min-lein-version "2.0.0"
   :uberjar-name "cabin.jar"
   :jvm-opts ["-server"]
-
 ;;enable to start the nREPL server when the application launches
 ;:env {:repl-port 7001}
-
   :main cabin.core
-
-  :plugins [
-            [lein-environ "1.0.0"]
-            [lein-ancient "0.6.5"]
-            ]
-  
-
-  
-  
-  
-  
+  :plugins [[lein-environ "1.0.0"]
+            [lein-ancient "0.6.5"]]
   :profiles
   {:uberjar {:omit-source true
              :env {:production true}
-             
              :aot :all}
    :dev {:dependencies [[ring-mock "0.1.5"]
                         [ring/ring-devel "1.3.2"]
                         [pjstadig/humane-test-output "0.7.0"]
                         ]
          :source-paths ["env/dev/clj"]
-         
-         
-         
          :repl-options {:init-ns cabin.core}
          :injections [(require 'pjstadig.humane-test-output)
                       (pjstadig.humane-test-output/activate!)]
